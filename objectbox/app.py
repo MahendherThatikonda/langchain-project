1750a533fd61ed48a42b17e1c0f61a9ec97140e8
MahendherThatikonda <116361201+MahendherThatikonda@users.noreply.github.com>
2026-03-12 20:39:45 +1300

    Added Chatgroq Document Q&A App and ObjectBox VectorDB function

---
+++ objectbox/app.py
@@ -0,0 +1,71 @@
+import streamlit as st
+import os
+from langchain_groq import ChatGroq
+from langchain_openai import OpenAIEmbeddings
+from langchain_classic.text_splitter import RecursiveCharacterTextSplitter
+from langchain_classic.chains.combine_documents import create_stuff_documents_chain
+from langchain_classic.chains import create_retrieval_chain
+from langchain_core.prompts import ChatPromptTemplate
+#from langchain_community.vectorstores import FAISS
+from langchain_objectbox.vectorstores import ObjectBox
+from langchain_community.document_loaders import PyPDFDirectoryLoader
+
+from dotenv import load_dotenv
+load_dotenv()
+
+## load the groq and openAI API key
+
+os.environ["OPENAI_API_KEY"] = os.getenv("OPENAI_API_KEY")
+groq_api_key=os.getenv("GROQ_API_KEY")
+
+st.title("ObjectBox Vector DB with Llama3 Demo")
+
+llm=ChatGroq(groq_api_key=groq_api_key,
+             model_name="Llama3-8b-8192")
+
+prompt=ChatPromptTemplate.from_template(
+    """
+Answer the questions based on the provided context only.
+Please provide the most accurate response based on the question.
+<context>
+{context}
+</context>
+Questions:{input}
+"""
+)
+
+def vector_embedding():
+
+    if "vector" not in st.session_state:
+        st.session_state.embeddings=OpenAIEmbeddings()
+        st.session_state.loader=PyPDFDirectoryLoader("./") #Data Ingestion
+        st.session_state.docs=st.session_state.loader.load() #Document Loading
+        st.session_state.text_splitter=RecursiveCharacterTextSplitter(chunk_size=1000,chunk_overlap=200) #Chunk creation
+        st.session_state.final_documents=st.session_state.text_splitter.split_documents(st.session_state.docs[:20]) #splitting
+        st.session_state.vectors=ObjectBox.from_documents(st.session_state.final_documents,st.session_state.embeddings,embedding_dimensions=768)  #vector OPenAI embeddings
+
+
+prmpt1=st.text_input("Enter your Question from the documents")
+
+if st.button("Documents Embedding"):
+    vector_embedding()
+    st.write("Vector Store DB is ready")
+
+
+import time
+
+document_chain=create_stuff_documents_chain(llm,prompt)
+retriever=st.session_state.vectors.as_retriever()
+retrieval_chain=create_retrieval_chain(retriever,document_chain)
+
+if prmpt1:
+    start=time.process_time()
+    response=retrieval_chain.invoke({'input':prmpt1})
+    print("Response Time:",time.process_time()-start)
+    st.write(response["answer"])
+
+
+    #With a streamlit explander
+    with st.expander("Document Similarity Search"):
+        for i,doc in enumerate(response["context"]):
+            st.write(doc.page_content)
